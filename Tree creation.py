f9e52df699ec2d574dc35dd548aacaa0954c66f2
Ishant_Chouhan <ishantchouhan666@gmail.com>
2024-07-14 13:01:30 +0530

    file update

--- Tree creation.py
+++ Tree creation.py
@@ -1,10 +1,10 @@
 class node:
-    def __init__(self,data):
-        self.data=data
+    def __init__(self,item):
+        self.item=item
         self.left=None
         self.right=None
     
-class BinaryTree:
+class BST:
     def __init__(self):
         self.root=None
 
@@ -13,15 +13,46 @@ class BinaryTree:
             self.root=newnode
         else:
             temp=self.root
-            while temp.link!=None:
-                if newnode.data>=temp.data:
+            while True:
+                if newnode.item>=temp.item and temp.right==None:
+                    temp.right=newnode
+                    print("Data inserted successfully...!!")
+                    break
+                elif newnode.item<temp.item and temp.left==None:
+                    temp.left=newnode
+                    print("Data inserted successfully...!!")
+                    break
+                elif newnode.item>=temp.item:
                     temp=temp.right
                 else:
                     temp=temp.left
 
-            if newnode.data>=temp.data:
-                temp.right=newnode
-            else:
-                temp.left=newnode
+    def search(self,element):
+        if self.root==None:
+            print("Oops...Tree is empty...!!")
+        else:
+            temp=self.root
+            while temp!=None:
+                if temp.item==element:
+                    print("search successfull...!!")
+                    break
+                elif element>temp.item:
+                    temp=temp.right
+                elif element<temp.item:
+                    temp=temp.left
+            if temp==None:
+                print("search unsuccessfull...!!")
+                
 
-    
+tree=BST()
+while True:
+    print("""press 1 to add element in tree
+press 2 to perform searching""")
+    choice=int(input("enter your choice: "))
+    if choice==1:
+        tree.add(node(int(input("Enter data: "))))
+    elif choice==2:
+        tree.search(int(input("Enter element to be searched: ")))  
+    else:
+        print("Exit...") 
+        break 
